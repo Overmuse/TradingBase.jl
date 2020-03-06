c2128615c754207fb949ddf65d297fd4a8fdc13c
Sebastian Rollen <rollenseb@gmail.com>
2020-03-05 21:01:36 -0500

    add Order

--- src/order.jl
+++ src/order.jl
@@ -49,3 +49,24 @@ stop_price(x::Union{StopOrder, StopLimitOrder}) = x.stop_price
 stop_price(o::AbstractOrder) = stop_price(o.type)
 
 should_trade(::AbstractOrderType) = error("Unimplemented")
+
+mutable struct Order{O <: AbstractOrderType, D <: AbstractOrderDuration} <: AbstractOrder
+    id::UUID
+    submitted_at::DateTime
+    filled_at::Union{DateTime, Nothing}
+    canceled_at::Union{DateTime, Nothing}
+    failed_at::Union{DateTime, Nothing}
+    symbol::String
+    quantity::Int
+    filled_quantity::Int
+    type::O
+    duration::D
+    limit_price::Union{Float64, Nothing}
+    stop_price::Union{Float64, Nothing}
+    filled_average_price::Union{Float64, Nothing}
+    commission::Union{Float64, Nothing}
+    status::String
+end
+
+is_filled(o::Order) = o.filled_quantity == o.quantity
+status(o::AbstractOrder) = o.status
