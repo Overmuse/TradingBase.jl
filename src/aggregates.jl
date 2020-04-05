833dce92cbbfcdca0bf40fed18dc66dbcc128704
Sebastian Rollen <rollenseb@gmail.com>
2020-04-05 19:11:48 -0400

    Sr/aggregate (#21)
    * aggregating
    
    * add conditions to volume aggregation
    
    * don't throw error at inconsistent high/low

--- src/aggregates.jl
+++ src/aggregates.jl
@@ -1,7 +1,4 @@
 abstract type AbstractMarketDataAggregate end
-struct InconsistentPriceException <: Exception
-    msg
-end
 
 struct Close{T <: Real} <: AbstractMarketDataAggregate
     close::T
@@ -13,15 +10,6 @@ struct OHLC{T <: Real} <: AbstractMarketDataAggregate
     close::T
 
     function OHLC(open, high, low, close)
-        if any(high .< [open, low, close])
-            throw(InconsistentPriceException(
-                "High is not the max in candle."
-            ))
-        elseif any(low .> [open, high, close])
-            throw(InconsistentPriceException(
-                "Low is not the min in candle."
-            ))
-        end
         promoted = promote(open, high, low, close)
         new{eltype(promoted)}(promoted...)
     end
@@ -34,15 +22,6 @@ struct OHLCV{T <: Real} <: AbstractMarketDataAggregate
     volume::Int
 
     function OHLCV(open, high, low, close, volume)
-        if any(high .< [open, low, close])
-            throw(InconsistentPriceException(
-                "High is not the max in candle."
-            ))
-        elseif any(low .> [open, high, close])
-            throw(InconsistentPriceException(
-                "Low is not the min in candle."
-            ))
-        end
         promoted = promote(open, high, low, close)
         new{eltype(promoted)}(promoted..., volume)
     end
